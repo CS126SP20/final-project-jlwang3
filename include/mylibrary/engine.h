a6dec29ba19188958370a58fc8f308f2148bd159
JeffreyWang <jlwang3@illinois.edu>
2020-04-23 14:02:45 -0400

    Engine.

--- include/mylibrary/engine.h
+++ include/mylibrary/engine.h
@@ -4,12 +4,19 @@
 
 #ifndef FINALPROJECT_ENGINE_H
 #define FINALPROJECT_ENGINE_H
+
+#include <cstdio>
+
 namespace mylibrary {
     /**
      * The engine should enforce the fact that pieces do not overlap and do not go outside the boundary.
      */
      class Engine {
+     public:
          void Reset();
+
+     private:
+         const size_t board_size_ = 10;
      };
 
 }
